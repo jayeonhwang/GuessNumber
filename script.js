6966d860d48b93b3b4be7c9dba125bc2f299fa5e
Jayeon Hwang <jayeon512@gmail.com>
2025-01-31 12:11:47 -0600

    set guess number enterring

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 'use strict';
 
+// document.querySelector(.className, #id, tag) - select CSS element and make it active
+//eventTarget.addEventListner (event(ex.'click'), function) - add event  
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '  ⛔️ No number!';
+  }
+});
+
+// set enter guess number event and type of enter number, add warning message for wrong format
