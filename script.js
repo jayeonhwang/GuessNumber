33c25d157454935d813b5ed1af3ed2f4275b0905
Jayeon Hwang <jayeon512@gmail.com>
2025-02-02 15:47:58 -0600

    set again button and store highscore

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ console.log(secretNumber);
 // set random number, between 1~20 (+1 for include 20) 
 // Math.trunc - ignor decimal 
 
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -42,6 +43,29 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 });
 
+document.querySelector('.again').addEventListener('click', function () {
+  let highScore = 0;
+
+  if (highScore < score) {
+    highScore = score;
+    document.querySelector('.highscore').textContent = highScore;
+  } else {
+    document.querySelector('.highscore').textContent = highScore;
+  }
+});
+
+
+
 // set enter guess number event and type of enter number, add warning message for wrong format
 // add messages for user 
 
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK 😀
+*/
